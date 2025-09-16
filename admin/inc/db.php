a861309be207bdf1177cb09505f3964f4f3004bb
mikejc673 <mikejc673@gmail.com>
2025-09-16 13:21:28 +0000

    db.php

---
+++ admin/inc/db.php
@@ -0,0 +1,25 @@
+<?php
+// inc/db.php
+// CONFIGUREZ ICI vos paramètres
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'epicerie');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_CHARSET', 'utf8mb4');
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO(
+        "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET,
+        DB_USER,
+        DB_PASS,
+        $options
+    );
+} catch (Exception $e) {
+    // En production, loggez et affichez un message générique
+    die("Erreur de connexion à la base de données.");
+}
